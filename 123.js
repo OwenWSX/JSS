f11fa797cf9c5f875badd2ff498560ad95857aca
OwenWSX <103395292+OwenWSX@users.noreply.github.com>
2022-04-19 17:25:38 +0800

    Add files via upload

---
+++ 123.js
@@ -0,0 +1,15 @@
+function total(){
+        var F=document.getElementById("F").value;
+        var D=document.getElementById("D").value;
+        var total=Number(F)+Number(D);
+        document.getElementById("result").innerHTML = total;
+        {
+        var F=document.getElementById("D").value;
+        if(F==""||isNaN(D))
+        
+        {
+        
+        alert("Not Numeric");
+    }
+}
+}
